c1a8215b01331cefcdd2da26cf905cd04192d6d4
Vanessa Nina <vninamore@gmail.com>
2021-07-24 12:44:02 -0500

    parcial

---
+++ parcial.R
@@ -0,0 +1,104 @@
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("ggplot2")
+library(ggplot2)
+install.packages("sf")
+library(sf)
+install.packages("raster")
+library(raster)
+library(rgdal)
+
+
+
+
+#1
+tangente_hiperbolica<-seq(-10,10,0.5)
+tangente_hiperbolica
+tanh<-function(a){
+  r<-(exp(2*a)-1)/(exp(2*a)+1)
+  return(r)
+}
+solucion<-tanh(tangente_hiperbolica)
+plot(tangente_hiperbolica,solucion,type="l")
+
+#2
+
+ecua_regresion<-function(v1,v2){
+  X1<-c()
+  Y2<-c()
+  for(i in 1:length(v1)){
+    x<-log10(v1[i])
+    y<-log10(v2[i])
+    X1<-c(X1,x)
+    Y2<-c(Y2,y)
+  }
+  multip<-X1*Y2
+  X1_alcuadrado<-X1^2
+  n<-length(v1)
+  B<-((n*sum(multip))-(sum(X1)*sum(Y2)))/((n*sum(X1_alcuadrado))-(sum(X1))^2)
+  A<-10^(mean(Y2)-B*mean(X1))
+  return(c(A,B))
+}
+x<-c(2.5,3,4,5,5.5,6,7)
+y<-c(12.5,10,7,4.5,4,3,3.5)
+ecua_regresion(x,y)
+
+#5
+CSSE<-function(v,r){
+  m<-matrix(v,ncol = 3)
+  ds<-det(m)
+  for (n in 1:length(r)) {
+    if(n==1){
+      m<-matrix(v,ncol = 3)
+      m[,n]<-r
+      dx<-det(m)
+    }else if(n==2){
+      m<-matrix(v,ncol = 3)
+      m[,n]<-r
+      dy<-det(m)
+    }else{
+      m<-matrix(v,ncol = 3)
+      m[,n]<-r
+      dz<-det(m)
+    }
+  }
+  x<-dx/ds
+  y<-dy/ds
+  z<-dz/ds
+  r<-c(x,y,z)
+  return(r)
+}
+variables_xyz<-c(2,5,1,1,-4,-1,3,1,-4)
+resul_de_SE<-c(7,-19,4)
+CSSE(variables_xyz,resul_de_SE)
+
+#PARTE2
+
+#1)
+
+setwd("D:/R/progra1/parcial")
+cuenca<- st_read("uh_datos.shp")
+
+plot(cuenca)
+datos <- readOGR(dsn = ".", layer= "uh_datos")
+data_nueva<-datos@data
+head(data_nueva)
+pp_prom<-data_nueva %>% 
+  group_by(AAA) %>% 
+  summarise(promedio_AAA=mean(pcp))
+pp_prom
+
+ggplot(pp_prom, aes(AAA, promedio_AAA)) +
+  geom_point(color = "purple")+
+  labs(y="promedio_AAA", x = "AAA")+
+  ggtitle("Precipitación promedio por Autoridad AAA")
+
+
+View(pp_prom)
+View(data_nueva)
+indic_ar<- data_nueva %>% 
+  dplyr::select(pcp, pet)
+View(indic_ar)
+indice<- indic_ar %>% 
+  summarise(indice=proportions(pcp, pet))
+#------------------------------
